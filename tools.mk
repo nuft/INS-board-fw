f4ce639044f52ee77ee25e4a6aba0128856f1c75
Michael Spieler <the.chaotic.coder@gmail.com>
2015-01-19 21:39:57 +0100

    Use correct project name for tools.mk include.

--- tools.mk
+++ tools.mk
@@ -1,6 +1,6 @@
 .PHONY: flash
 flash: all
-	openocd -f oocd.cfg -c "program build/ins-board.elf verify reset"
+	openocd -f oocd.cfg -c "program build/$(PROJECT).elf verify reset"
 
 .PHONY: r
 r: reset
