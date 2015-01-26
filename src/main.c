cd50ab4c10b4ac51f168e722ec13109354d4ed30
Michael Spieler <the.chaotic.coder@gmail.com>
2015-01-26 18:52:38 +0100

    Clean main.c, move shell commands to separate file.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,14 @@
-#include "ch.h"
-#include "hal.h"
-#include "chprintf.h"
-#include "shell.h"
-
+#include <ch.h>
+#include <hal.h>
+#include <chprintf.h>
 #include <string.h>
 #include "usbcfg.h"
-
 #include "sensors/onboardsensors.h"
-#include "sensors/ms5611.h"
 #include "serial-datagram/serial_datagram.h"
 #include "cmp/cmp.h"
+#include "shell_commands.h"
 
 SerialUSBDriver SDU1;
-
 
 /*
  *  Heartbeat, Error LED thread
@@ -58,104 +54,6 @@ static THD_FUNCTION(led_task, arg)
     }
     return 0;
 }
-
-
-#define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
-static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
-    size_t n, size;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: mem\r\n");
-        return;
-    }
-    n = chHeapStatus(NULL, &size);
-    chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
-    chprintf(chp, "heap fragments   : %u\r\n", n);
-    chprintf(chp, "heap free total  : %u bytes\r\n", size);
-}
-
-static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
-    static const char *states[] = {CH_STATE_NAMES};
-    thread_t *tp;
-
-    (void)argv;
-    if (argc > 0) {
-        chprintf(chp, "Usage: threads\r\n");
-        return;
-    }
-    chprintf(chp, "    addr    stack prio refs     state\r\n");
-    tp = chRegFirstThread();
-    do {
-        chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
-                 (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
-                 (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-                 states[tp->p_state]);
-        tp = chRegNextThread(tp);
-    } while (tp != NULL);
-}
-
-
-static void cmd_gyro(BaseSequentialStream *chp, int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
-    int i;
-    for (i = 0; i < 100; i++) {
-        chSysLock();
-        int gx = 1000*mpu_gyro_sample.rate[0];
-        int gy = 1000*mpu_gyro_sample.rate[1];
-        int gz = 1000*mpu_gyro_sample.rate[2];
-        chSysUnlock();
-        chprintf(chp, "gyro %d %d %d\n", gx, gy, gz);
-        chThdSleepMilliseconds(10);
-    }
-}
-
-static void cmd_baro(BaseSequentialStream *chp, int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
-    int i;
-    for (i = 0; i < 100; i++) {
-        int32_t t;
-        uint32_t p;
-        chSysLock();
-        t = barometer_temperature;
-        p = barometer_pressure;
-        chSysUnlock();
-        chprintf(chp, "baro p: %d t:%d\n", p, t);
-        chThdSleepMilliseconds(10);
-    }
-}
-
-
-static void cmd_dfu(BaseSequentialStream *chp, int argc, char *argv[])
-{
-    (void)argc;
-    (void)argv;
-
-    chprintf(chp, "rebooting in 3 seconds...\n");
-    chThdSleepMilliseconds(3000);
-    usbDisconnectBus(serusbcfg.usbp);
-    chThdSleepMilliseconds(100);
-    reboot_and_run_bootloader();
-}
-
-static const ShellCommand commands[] = {
-  {"mem", cmd_mem},
-  {"threads", cmd_threads},
-  {"gyro", cmd_gyro},
-  {"baro", cmd_baro},
-  {"dfu", cmd_dfu},
-  {NULL, NULL}
-};
-
-static const ShellConfig shell_cfg1 = {
-  (BaseSequentialStream *)&SDU1,
-  commands
-};
-
 
 static void _stream_imu_values_sndfn(void *arg, const void *p, size_t len)
 {
@@ -222,17 +120,16 @@ void stream_imu_values(BaseSequentialStream *out)
 int main(void)
 {
     // check_bootloader();
-
     halInit();
     chSysInit();
-
-    chThdCreateStatic(led_task_wa, sizeof(led_task_wa), LOWPRIO, led_task, NULL);
-
-    enable_charging();
 
     // debug UART
     sdStart(&UART_CONN1, NULL);
     chprintf((BaseSequentialStream*)&UART_CONN1, "\n> start\n");
+
+    chThdCreateStatic(led_task_wa, sizeof(led_task_wa), LOWPRIO, led_task, NULL);
+
+    enable_charging();
 
     // sensors
     onboard_sensors_start();
@@ -244,25 +141,15 @@ int main(void)
     chThdSleepMilliseconds(1000);
     usbStart(serusbcfg.usbp, &usbcfg);
     usbConnectBus(serusbcfg.usbp);
-
     // while (SDU1.config->usbp->state != USB_ACTIVE) {
     //     chThdSleepMilliseconds(10);
     // }
+
     // stream_imu_values((BaseSequentialStream*)&UART_CONN1);
 
-    shellInit();
-    thread_t *shelltp = NULL;
     while (true) {
-        if (!shelltp) {
-            if (SDU1.config->usbp->state == USB_ACTIVE) {
-                shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
-            }
-        } else {
-            if (chThdTerminatedX(shelltp)) {
-                chThdRelease(shelltp);
-                shelltp = NULL;
-            }
-        }
+        // shell_run((BaseSequentialStream*)&SDU1);
+        shell_run((BaseSequentialStream*)&UART_CONN1);
         chThdSleepMilliseconds(500);
     }
 }
---
+++ src/shell_commands.c
@@ -0,0 +1,108 @@
+#include <ch.h>
+#include <chprintf.h>
+#include "board.h"
+#include "shell.h"
+#include "sensors/onboardsensors.h"
+#include "shell_commands.h"
+
+#define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
+
+static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    size_t n, size;
+
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: mem\r\n");
+        return;
+    }
+    n = chHeapStatus(NULL, &size);
+    chprintf(chp, "core free memory : %u bytes\r\n", chCoreGetStatusX());
+    chprintf(chp, "heap fragments   : %u\r\n", n);
+    chprintf(chp, "heap free total  : %u bytes\r\n", size);
+}
+
+static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    static const char *states[] = {CH_STATE_NAMES};
+    thread_t *tp;
+
+    (void)argv;
+    if (argc > 0) {
+        chprintf(chp, "Usage: threads\r\n");
+        return;
+    }
+    chprintf(chp, "    addr    stack prio refs     state\r\n");
+    tp = chRegFirstThread();
+    do {
+        chprintf(chp, "%08lx %08lx %4lu %4lu %9s\r\n",
+                 (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+                 (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
+                 states[tp->p_state]);
+        tp = chRegNextThread(tp);
+    } while (tp != NULL);
+}
+
+static void cmd_sensors(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    int count = 100;
+    while (count-- > 0) {
+        float g[3], a[3];
+        uint32_t p;
+
+        chSysLock();
+        g[0] = mpu_gyro_sample.rate[0];
+        g[1] = mpu_gyro_sample.rate[1];
+        g[2] = mpu_gyro_sample.rate[2];
+        a[0] = mpu_acc_sample.acceleration[0];
+        a[1] = mpu_acc_sample.acceleration[1];
+        a[2] = mpu_acc_sample.acceleration[2];
+        p = barometer_pressure;
+        chSysUnlock();
+
+        chprintf(chp, "gyro: %d %d %d, acc: %d %d %d, baro: %u\n",
+            (int)(1000*g[0]),
+            (int)(1000*g[1]),
+            (int)(1000*g[2]),
+            (int)(1000*a[0]),
+            (int)(1000*a[1]),
+            (int)(1000*a[2]),
+            (unsigned int)(p));
+        chThdSleepMilliseconds(100);
+    }
+}
+
+static void cmd_dfu(BaseSequentialStream *chp, int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    chprintf(chp, "rebooting in 3 seconds...\n");
+    chThdSleepMilliseconds(3000);
+    reboot_and_run_bootloader();
+}
+
+static const ShellCommand commands[] = {
+    {"mem", cmd_mem},
+    {"threads", cmd_threads},
+    {"sensors", cmd_sensors},
+    {"dfu", cmd_dfu},
+    {NULL, NULL}
+};
+
+void shell_run(BaseSequentialStream *stream)
+{
+    ShellConfig shell_cfg1 = {
+        stream,
+        commands
+    };
+    shellInit();
+    thread_t *shelltp = shellCreate(&shell_cfg1, SHELL_WA_SIZE, NORMALPRIO);
+    while (!chThdTerminatedX(shelltp)) {
+        chThdSleepMilliseconds(10);
+    }
+    chThdRelease(shelltp);
+}
