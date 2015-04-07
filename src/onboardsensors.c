5b407ccd1e2d30fce536a0c9ce81bbac30f4efbf
Michael Spieler <the.chaotic.coder@gmail.com>
2015-04-07 23:38:53 +0200

    Update sensors as in INS-board.

--- src/onboardsensors.c
+++ src/onboardsensors.c
@@ -1,29 +1,28 @@
 #include <math.h>
 #include <ch.h>
-#include "mpu60X0.h"
-#include "ms5611.h"
-#include "imu.h"
+#include "sensors/mpu60X0.h"
+#include "sensors/ms5611.h"
+#include "timestamp/timestamp.h"
+#include "sensors.h"
 #include "exti.h"
 
-rate_gyro_sample_t mpu_gyro_sample;
-accelerometer_sample_t mpu_acc_sample;
+rate_gyro_sample_t gyro_sample;
+accelerometer_sample_t acc_sample;
+barometer_sample_t barometer_sample;
 
-uint32_t barometer_pressure;
-int32_t barometer_temperature;
+#define MPU_IRQ_EVENT_MASK 1
 
-#define SENSOR_INTERRUPT_EVENT 1
-
-static THD_WORKING_AREA(i2c_sensors_critical_wa, 512);
-static THD_FUNCTION(i2c_sensors_critical, arg)
+static THD_WORKING_AREA(imu_sensors_wa, 512);
+static THD_FUNCTION(imu_sensors, arg)
 {
-    chRegSetThreadName("i2c sensors critical");
+    chRegSetThreadName("imu sensors");
 
     I2CDriver *driver = (I2CDriver *)arg;
     static mpu60X0_t mpu6050;
 
-    static event_listener_t sensor_int;
-    chEvtRegisterMaskWithFlags(&exti_events, &sensor_int,
-                               (eventmask_t)SENSOR_INTERRUPT_EVENT,
+    static event_listener_t mpu_irq_listener;
+    chEvtRegisterMaskWithFlags(&exti_events, &mpu_irq_listener,
+                               (eventmask_t)MPU_IRQ_EVENT_MASK,
                                (eventflags_t)EXTI_EVENT_MPU6050_INT);
 
     i2cAcquireBus(driver);
@@ -35,46 +34,46 @@ static THD_FUNCTION(i2c_sensors_critical, arg)
         return 0;
     }
 
-    mpu60X0_setup(&mpu6050, MPU60X0_SAMPLE_RATE_DIV(0) | MPU60X0_ACC_FULL_RANGE_2G
-        | MPU60X0_GYRO_FULL_RANGE_500DPS | MPU60X0_LOW_PASS_FILTER_6);
+    mpu60X0_setup(&mpu6050, MPU60X0_SAMPLE_RATE_DIV(0) | MPU60X0_ACC_FULL_RANGE_4G
+        | MPU60X0_GYRO_FULL_RANGE_1000DPS | MPU60X0_LOW_PASS_FILTER_6);
     i2cReleaseBus(driver);
 
     static rate_gyro_t mpu_gyro = {
-        .device = "MPU6050", .full_scale_range = {500*M_PI/360, 500*M_PI/360, 500*M_PI/360},
-        .noise_stddev = {NAN, NAN, NAN}, .update_rate = 8000, .health = SENSOR_HEALTH_OK };
+        .device = "MPU6050", .full_scale_range = 1000*M_PI/180, .update_rate = 1000};
     static accelerometer_t mpu_acc = {
-        .device = "MPU6050", .full_scale_range = {2*9.81, 2*9.81, 2*9.81},
-        .noise_stddev = {NAN, NAN, NAN}, .update_rate = 8000, .health = SENSOR_HEALTH_OK };;
-    mpu_gyro_sample.sensor = &mpu_gyro;
-    mpu_acc_sample.sensor = &mpu_acc;
-
+        .device = "MPU6050", .full_scale_range = 4*9.81, .update_rate = 1000};;
+    gyro_sample.sensor = &mpu_gyro;
+    acc_sample.sensor = &mpu_acc;
 
     while (1) {
         float gyro[3], acc[3], temp;
-        chEvtWaitAny(SENSOR_INTERRUPT_EVENT);
+        chEvtWaitAny(MPU_IRQ_EVENT_MASK);
+        timestamp_t t = timestamp_get();
         i2cAcquireBus(driver);
         mpu60X0_read(&mpu6050, gyro, acc, &temp);
         i2cReleaseBus(driver);
 
         chSysLock();
-        mpu_gyro_sample.rate[0] = gyro[0];
-        mpu_gyro_sample.rate[1] = gyro[1];
-        mpu_gyro_sample.rate[2] = gyro[2];
-        mpu_acc_sample.acceleration[0] = acc[0];
-        mpu_acc_sample.acceleration[1] = acc[1];
-        mpu_acc_sample.acceleration[2] = acc[2];
+        gyro_sample.timestamp = t;
+        gyro_sample.rate[0] = gyro[0];
+        gyro_sample.rate[1] = gyro[1];
+        gyro_sample.rate[2] = gyro[2];
+        acc_sample.timestamp = t;
+        acc_sample.acceleration[0] = acc[0];
+        acc_sample.acceleration[1] = acc[1];
+        acc_sample.acceleration[2] = acc[2];
         chSysUnlock();
     }
     return 0;
 }
 
-static THD_WORKING_AREA(i2c_sensors_standard_wa, 512);
-static THD_FUNCTION(i2c_sensors_standard, arg)
+static THD_WORKING_AREA(barometer_thread_wa, 512);
+static THD_FUNCTION(barometer_thread, arg)
 {
     I2CDriver *driver = (I2CDriver *)arg;
     static ms5611_t ms5611;
 
-    chRegSetThreadName("i2c sensors standard");
+    chRegSetThreadName("barometer thread");
 
     i2cAcquireBus(driver);
     if (ms5611_i2c_init(&ms5611, driver, 0)) {
@@ -90,37 +89,36 @@ static THD_FUNCTION(i2c_sensors_standard, arg)
     while (1) {
         uint32_t raw_t, raw_p, press;
         int32_t temp;
-        int16_t delay;
+        int16_t t;
 
         i2cAcquireBus(driver);
-        delay = ms5611_adc_start(&ms5611, MS5611_ADC_TEMP, MS5611_OSR_4096);
+        t = ms5611_adc_start(&ms5611, MS5611_ADC_TEMP, MS5611_OSR_4096);
         i2cReleaseBus(driver);
-        if (delay < 0) {
-            continue;
+        if (t > 0) {
+            chThdSleepMilliseconds((t - 1)/1000 + 1);
+            i2cAcquireBus(driver);
+            ms5611_adc_read(&ms5611, &raw_t);
+            i2cReleaseBus(driver);
         } else {
-            chThdSleepMilliseconds((delay - 1)/1000 + 1);
+            continue;
         }
 
         i2cAcquireBus(driver);
-        ms5611_adc_read(&ms5611, &raw_t);
-        delay = ms5611_adc_start(&ms5611, MS5611_ADC_PRESS, MS5611_OSR_4096);
+        t = ms5611_adc_start(&ms5611, MS5611_ADC_PRESS, MS5611_OSR_4096);
         i2cReleaseBus(driver);
-
-        if (delay < 0) {
-            continue;
+        if (t > 0) {
+            chThdSleepMilliseconds((t - 1)/1000 + 1);
+            i2cAcquireBus(driver);
+            ms5611_adc_read(&ms5611, &raw_p);
+            i2cReleaseBus(driver);
         } else {
-            chThdSleepMilliseconds((delay - 1)/1000 + 1);
+            continue;
         }
-
-        i2cAcquireBus(driver);
-        ms5611_adc_read(&ms5611, &raw_p);
-        i2cReleaseBus(driver);
-
         press = ms5611_calc_press(&ms5611, raw_p, raw_t, &temp);
 
         chSysLock();
-        barometer_pressure = press;
-        barometer_temperature = temp;
+        barometer_sample.pressure = press;
+        barometer_sample.temperature = (float)temp/100;
         chSysUnlock();
 
         chThdSleepMilliseconds(100);
@@ -130,8 +128,6 @@ static THD_FUNCTION(i2c_sensors_standard, arg)
 
 void onboard_sensors_start(void)
 {
-    exti_setup();
-
     /*
      * I2C setup
      */
@@ -144,6 +140,6 @@ void onboard_sensors_start(void)
 
     i2cStart(driver, &i2c_cfg);
 
-    chThdCreateStatic(i2c_sensors_critical_wa, sizeof(i2c_sensors_critical_wa), NORMALPRIO, i2c_sensors_critical, driver);
-    chThdCreateStatic(i2c_sensors_standard_wa, sizeof(i2c_sensors_standard_wa), LOWPRIO, i2c_sensors_standard, driver);
+    chThdCreateStatic(imu_sensors_wa, sizeof(imu_sensors_wa), NORMALPRIO, imu_sensors, driver);
+    chThdCreateStatic(barometer_thread_wa, sizeof(barometer_thread_wa), LOWPRIO, barometer_thread, driver);
 }
--- src/onboardsensors.h
+++ src/onboardsensors.h
@@ -1,13 +1,11 @@
 #ifndef ONBOARDSENSORS_H
 #define ONBOARDSENSORS_H
 
-#include "imu.h"
+#include "sensors.h"
 
-extern rate_gyro_sample_t mpu_gyro_sample;
-extern accelerometer_sample_t mpu_acc_sample;
-
-extern uint32_t barometer_pressure;
-extern int32_t barometer_temperature;
+extern rate_gyro_sample_t gyro_sample;
+extern accelerometer_sample_t acc_sample;
+extern barometer_sample_t barometer_sample;
 
 void onboard_sensors_start(void);
 
